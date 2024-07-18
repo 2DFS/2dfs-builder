e0e96a9a4caf8fa3cc0f2dcfda19f7a864b00d6e
giobarty <giobarty@gmail.com>
2024-07-18 15:50:41 +0200

    created experiment benchmark

---
+++ examples/benchmark/README.md
@@ -0,0 +1,10 @@
+#How to run the test
+
+## Requirements
+
+Install docker
+Ensure sudoless docker:
+    - Follow: https://docs.docker.com/engine/install/linux-postinstall/
+    - ensure permission to .docker folder with `sudo chown -R $(whoami) ~/.docker` 
+
+Install tdfs with the ./install script 
--- examples/benchmark/benchmark.py
+++ examples/benchmark/benchmark.py
@@ -1,8 +1,10 @@
 import os
 from random import getrandbits
 import subprocess
+import json
 
 
+REPEAT = 5
 EXPERIMENT_ALLOTMENT_RATIO = [
     {
         "size": 100,
@@ -37,13 +39,55 @@ def create_random_file(file_size_mb, filename):
             # Update remaining bytes
             file_size_bytes -= 1024 * 1024
 
+def gen_2dfs_manifest(files_list):
+    manifest = {
+        "allotments":[]
+    }
+    i = 0
+    for f in files_list:
+        manifest["allotments"].append({
+            "src":f,
+            "dst":"/file"+str(i),
+            "row":i,
+            "col":i
+        })
+        i += 1
+
+    #delete 2dfs.json file if it exists
+    if os.path.exists("2dfs.json"):
+        os.remove("2dfs.json")
+
+    #write manifest to 2dfs.json file
+    with open("2dfs.json", "w") as f:
+        json.dump(manifest, f)
+
+def gen_dockerfile(files_list):
+    dockerfile = "FROM ubuntu:22.04\n"
+    i = 0
+    for f in files_list:
+        dockerfile += "COPY "+f+" /file"+str(i)+"\n"
+        i += 1
+
+    #delete 2dfs.json file if it exists
+    if os.path.exists("Dockerfile"):
+        os.remove("Dockerfile")
+
+    #write manifest to 2dfs.json file
+    with open("Dockerfile", "w") as f:
+        f.write(dockerfile)
+
 def cleanup_dir(dir):
     for file in os.listdir(dir):
         os.remove(os.path.join(dir, file))
 
 
 def build_tdfs():
-   pass
+   cmd = ["tdfs", "build", "ubuntu:22.04","test:v1","--platforms", "linux/amd64"]
+   execute_with_live_output(cmd)
+
+def build_docker():
+   cmd = ["docker", "build", "-t","test:v1","."]
+   execute_with_live_output(cmd)
 
 def exec_command(command):
     try:
@@ -74,7 +118,45 @@ def execute_with_live_output(cmd):
     process.wait()
 
 def cleanup_tdfs():
-    pass
+    cmd = ["tdfs", "image", "rm","-a"]
+    execute_with_live_output(cmd)
+
+def cleanup_docker():
+    cmd = ["docker", "system", "prune","-a","-f"]
+    execute_with_live_output(cmd)
 
 if __name__ == "__main__":
-    execute_with_live_output(["ls","-al"])
+    cleanup_tdfs()
+    cleanup_docker()
+    
+    for e in EXPERIMENT_ALLOTMENT_RATIO:
+
+        print("\n ##EXPERIMENT CONFIG ## \n",str(e))
+
+        for r in range(REPEAT):
+
+            print("\n ##EXPERIMENT RUN ",r,"## \n")
+        
+            
+            files_list = []
+            for j in range(e["allotments"]):
+                filename = "files/f"+str(j)
+                create_random_file(e["size"], filename)
+                files_list.append(filename)
+
+            ## TDFS EXPERIMENT
+            # Generate 2dfs manifest
+            print("###TDFS EXPERIMENT##")
+            gen_2dfs_manifest(files_list)
+            build_tdfs()
+            cleanup_tdfs()
+
+            ## DOCKER EXPERIMENT
+            print("###DOCKER EXPERIMENT##")
+            gen_dockerfile(files_list)
+            build_docker()
+            cleanup_docker()
+
+
+            ## cleanup files
+            cleanup_dir("./files")
